e58e030c5900c12118bb76cc169b879786d5568b
sophia.wang <wangpp@chinaskycloud.com>
2017-03-22 15:29:10 +0800

    first commit

---
+++ src/app/pages/pages.menu.ts
@@ -0,0 +1,163 @@
+export const PAGES_MENU = [
+  {
+    path: 'pages',
+    children: [
+      {
+        path: 'dashboard',
+        data: {
+          menu: {
+            title: 'Dashboard',
+            icon: 'ion-android-home',
+            selected: false,
+            expanded: false,
+            order: 0
+          }
+        }
+      },
+      {
+        path: 'ui',
+        data: {
+          menu: {
+            title: 'UI Features',
+            icon: 'ion-android-laptop',
+            selected: false,
+            expanded: false,
+            order: 300,
+          }
+        },
+        children: [
+          {
+            path: 'typography',
+            data: {
+              menu: {
+                title: 'Typography',
+              }
+            }
+          },
+          {
+            path: 'buttons',
+            data: {
+              menu: {
+                title: 'Buttons',
+              }
+            }
+          },
+          {
+            path: 'icons',
+            data: {
+              menu: {
+                title: 'Icons',
+              }
+            }
+          },
+          {
+            path: 'modals',
+            data: {
+              menu: {
+                title: 'Modals',
+              }
+            }
+          },
+          {
+            path: 'grid',
+            data: {
+              menu: {
+                title: 'Grid',
+              }
+            }
+          },
+        ]
+      },
+      {
+        path: 'forms',
+        data: {
+          menu: {
+            title: 'Form Elements',
+            icon: 'ion-compose',
+            selected: false,
+            expanded: false,
+            order: 400,
+          }
+        },
+        children: [
+          {
+            path: 'inputs',
+            data: {
+              menu: {
+                title: 'Form Inputs',
+              }
+            }
+          },
+          {
+            path: 'layouts',
+            data: {
+              menu: {
+                title: 'Form Layouts',
+              }
+            }
+          }
+        ]
+      },
+      {
+        path: 'tables',
+        data: {
+          menu: {
+            title: 'Tables',
+            icon: 'ion-grid',
+            selected: false,
+            expanded: false,
+            order: 500,
+          }
+        },
+        children: [
+          {
+            path: 'basictables',
+            data: {
+              menu: {
+                title: 'Basic Tables',
+              }
+            }
+          },
+          {
+            path: 'smarttables',
+            data: {
+              menu: {
+                title: 'Smart Tables',
+              }
+            }
+          }
+        ]
+      },
+      {
+        path: '',
+        data: {
+          menu: {
+            title: 'Pages',
+            icon: 'ion-document',
+            selected: false,
+            expanded: false,
+            order: 650,
+          }
+        },
+        children: [
+          {
+            path: ['/login'],
+            data: {
+              menu: {
+                title: 'Login'
+              }
+            }
+          },
+          {
+            path: ['/register'],
+            data: {
+              menu: {
+                title: 'Register'
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
+];
